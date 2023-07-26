25898468680c84050b79a035a095fca9f5ba3766
Mayhem-XD <leahblo1994@gmail.com>
2023-07-26 15:27:51 +0900

    users

--- src/main/java/com/ys/sbbs/controller/UserController.java
+++ src/main/java/com/ys/sbbs/controller/UserController.java
@@ -1,22 +1,27 @@
 package com.ys.sbbs.controller;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.ys.sbbs.entity.User;
 import com.ys.sbbs.service.UserService;
+import com.ys.sbbs.utility.AsideUtil;
 
 @Controller
 @RequestMapping("/user")
@@ -24,6 +29,8 @@ import com.ys.sbbs.service.UserService;
 public class UserController {
 	@Autowired private UserService userService;
 	// Get 입력 폼 보여주기 Post 입력 값 파라메터 -> DB
+	
+	@Value("${spring.servlet.multipart.location}") private String uploadDir;
 	
 	@GetMapping("/register")
 	public String registerForm() {
@@ -36,7 +43,7 @@ public class UserController {
 		String pwd2 = req.getParameter("pwd2");
 		String uname = req.getParameter("uname");
 		String email = req.getParameter("email");
-//		String filePart = req.getPart("profile");
+		MultipartFile filePart =req.getFile("profile");
 		String addr = req.getParameter("addr");
 		
 		if (userService.getUser(uid) != null) {
@@ -47,12 +54,23 @@ public class UserController {
 		
 		if (pwd.equals(pwd2) && pwd.length()>=1) {
 			String hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
-			User user = new User(uid, hashedPwd, uname, email, null, addr);
+			String filename = null;
+			if (filePart != null) {
+				filename = filePart.getOriginalFilename();
+				String profilePath = uploadDir + "profile/" + filename;
+				try {
+					filePart.transferTo(new File(profilePath));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			AsideUtil au = new AsideUtil();
+			filename = au.squareImage(uploadDir + "profile/", filename);
+			User user = new User(uid, hashedPwd, uname, email, filename, addr);
 			userService.insertUser(user);
 			model.addAttribute("msg", "등록을 마쳤습니다. 로그인 하세요");
 			model.addAttribute("url", "/sbbs/user/login");
 			return "common/alertMsg";
-			
 			
 		} else {
 			
@@ -68,7 +86,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/login")
-	public String loginProc(String uid, String pwd, HttpSession session, Model model) {
+	public String loginProc(String uid, String pwd, HttpSession session, Model model, HttpServletRequest req) {
 		
 		int result = userService.login(uid, pwd);
 		if (result == UserService.CORRECT_LOGIN) {
@@ -79,15 +97,12 @@ public class UserController {
 			session.setAttribute("addr", user.getAddr());
 			session.setAttribute("profile", user.getProfile());
 			// 상태 메세지
-//			String quoteFile = getServletContext().getRealPath("/") + "WEB-INF/data/todayQuote.txt";
-//			AsideUtil au = new AsideUtil();
-//			String stateMsg = au.getTodayQuote(quoteFile);
-//			session.setAttribute("stateMsg", stateMsg);
-			
-			
+			String quoteFile = uploadDir + "data/todayQuote.txt";
+			AsideUtil au = new AsideUtil();
+			String stateMsg = au.getTodayQuote(quoteFile);
+			session.setAttribute("stateMsg", stateMsg);
 			// 환영 메세지
 			model.addAttribute("msg", user.getUname() + "님 환영합니다.");
-//			model.addAttribute("url", "/sbbs/board/list?p=1&f=&q=");
 			model.addAttribute("url", "/sbbs/user/list/1");
 			return "common/alertMsg";
 		} else if (result == UserService.WRONG_PASSWORD) {
@@ -128,10 +143,69 @@ public class UserController {
 		model.addAttribute("user", user);
 		return "/user/update";
 	}
-	@PostMapping("/update/{uid}")
-	public String update(@PathVariable String uid) {
-		return null;
+	@PostMapping("/update")
+	public String updateProc(MultipartHttpServletRequest req, HttpSession session, Model model) {
+		String uid = req.getParameter("uid");
+		String hashedPwd = req.getParameter("hashedPwd");
+		String oldFilename = req.getParameter("filename");
+		String pwd = req.getParameter("pwd");
+		String pwd2 = req.getParameter("pwd2");
+		String uname = req.getParameter("uname");
+		String email = req.getParameter("email");
+		MultipartFile filePart = req.getFile("profile");
+		String addr = req.getParameter("addr");
+		
+		boolean pwdFlag = false;
+		if (pwd != null && pwd.length() > 1 && pwd.equals(pwd2)) {
+			hashedPwd = BCrypt.hashpw(pwd, BCrypt.gensalt());
+			pwdFlag = true;
+		}
+		
+		String filename = null;
+		if (filePart != null) {			// 새로운 이미지로 변경
+			if (oldFilename != null) {	// 기존 이미지 삭제
+				File oldFile = new File(uploadDir + "profile/" + oldFilename);
+				oldFile.delete();
+//				이미지 저장하고, 스퀘어 이미지로 변경 register code와 동일
+				filename = filePart.getOriginalFilename();
+				String profilePath = uploadDir + "profile/" + filename;
+				try {
+					filePart.transferTo(new File(profilePath));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				AsideUtil au = new AsideUtil();
+				filename = au.squareImage(uploadDir + "profile/", filename);
+			} else 
+				filename = oldFilename;
+		}
+		
+		User user = new User(uid, hashedPwd, uname, email, filename, addr);
+		userService.updateUser(user);
+		session.setAttribute("uname", uname);
+		session.setAttribute("email", email);
+		session.setAttribute("addr", addr);
+		session.setAttribute("profile", filename);
+		if (pwdFlag) {
+			model.addAttribute("msg", "패스워드가 변경 되었습니다.");
+			model.addAttribute("url", "/sbbs/user/list/" + session.getAttribute("currentUserPage"));
+			return "common/alertMsg";
+		} else {
+			return "redirect:/user/list/" + session.getAttribute("currentUserPage");
+		}
+		
 	}
+	@GetMapping("/delete/{uid}")
+	public String delete(@PathVariable String uid, Model model) {
+		model.addAttribute("uid", uid);
+		return "user/delete";
+	}
+	@GetMapping("/deleteConfirm/{uid}")
+	public String deleteConfirm(@PathVariable String uid, HttpSession session) {
+		userService.deleteUser(uid);
+		return "redirect:/user/list/" + session.getAttribute("currentUserPage");
+	}
+	
 	
 
 }
